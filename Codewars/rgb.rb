2cc70d7bdbb65e05e199ec433432469d8519364c
Isabel <iarribas01@gmail.com>
2022-03-30 12:00:33 +0100

    More codewars

--- Codewars/rgb.rb
+++ Codewars/rgb.rb
@@ -25,14 +25,23 @@ How to convert dec to hex
 =end
 
 def rgb(r, g, b)
+  # [r, g, b].map do |decimal|
+  #   decimal = 255 if decimal > 255
+  #   decimal == 0 ? hexadecimal = '00' : hexadecimal = ''
+  #   until decimal == 0
+  #     decimal, digit = decimal.divmod(16)
+  #     hexadecimal.prepend(digit.to_s(16))
+  #   end
+  #   hexadecimal.upcase
+  # end.join
   [r, g, b].map do |decimal|
-    decimal = 255 if decimal > 255
-    decimal == 0 ? hexadecimal = '00' : hexadecimal = ''
-    until decimal == 0
-      decimal, digit = decimal.divmod(16)
-      hexadecimal.prepend(digit.to_s(16))
+    if decimal > 255
+      'FF'
+    elsif decimal < 0
+      '00'
+    else
+      decimal.to_s(16).upcase.rjust(2, '0')
     end
-    hexadecimal.upcase
   end.join
 end
 
@@ -45,6 +54,7 @@ p rgb(255, 255, 255) ==  'FFFFFF'
 p rgb(255, 255, 300) ==  'FFFFFF'
 p rgb(0,0,0) ==  '000000'
 p rgb(148, 0, 211) ==  '9400D3'
+p rgb(0, 0, -20) == '000000'
 
 
   
